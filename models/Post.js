72244bf1c5db2f6561956993a03b1aac3937d85e
Amy Wowak <akwowak@gmail.com>
2016-09-15 20:28:29 -0400

    slight mods

--- models/Post.js
+++ models/Post.js
@@ -11,6 +11,7 @@ module.exports = function(sequelize, DataTypes) {
                         allowNull: false
                     }
                 });
+                Post.hasMany(Comment);
             }
         }
     });
