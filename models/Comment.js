4e7124d3a00668279fb8f09d078019f60e0fc61b
Amy Wowak <akwowak@gmail.com>
2016-09-18 07:49:11 -0400

    changes to comments

--- models/Comment.js
+++ models/Comment.js
@@ -2,25 +2,16 @@ module.exports = function(sequelize, DataTypes) {
     var Comment = sequelize.define('comments', {
         content: DataTypes.TEXT
     }, {
-      classMethods: {
-        associate: function(models){
-          Comment.belongsTo(models.posts, {
-            onDelete: 'CASCADE',
-            foreignKey: {
-              allowNull: false
+        classMethod: {
+            associate: function(models) {
+                Comment.belongsTo(models.posts, {
+                    onDelete: 'CASCADE',
+                    foreignKey: {
+                        allowNull: false
+                    }
+                });
             }
-          });
-
-          Comment.belongsTo(models.users, {
-            onDelete: 'SET NULL',
-            foreignKey: {
-              allowNull: true
-              // a comment doesn't always belong to
-              // a user (can be anonymous)
-            }
-          });
         }
-      }
     });
 
     return Comment;
